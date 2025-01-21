650d11c0b0e4a78687bd4fe8e05f0eb4a2d67e39
aaronsabilano <aaron.sabilano@georgebrown.ca>
2025-01-21 11:15:58 -0500

    fix comments for dice and weapons

--- lab03.py
+++ lab03.py
@@ -1,8 +1,12 @@
 import random
 
-# Setup
+# Dice options using list() and range()
 diceOptions = list(range(1, 7))
+
+# Weapons Array
 weapons = ['Fist', 'Knife', 'Club', 'Gun', 'Bomb', 'Nuclear Bomb']
+
+
 print("Available Weapons:", ', '.join(weapons))
 
 # Inputs
